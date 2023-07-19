65fdbf49306803ca2c0e220462dad0e009964e1e
Davi Menezes <davimenezes.dev@gmail.com>
2023-07-19 00:07:14 -0300

    moving livewire components to inside module

---
+++ Http/Livewire/Pages/TasksPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Task\Http\Livewire\Pages;
+
+use Livewire\Component;
+
+class TasksPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.tasks-page');
+    }
+}
