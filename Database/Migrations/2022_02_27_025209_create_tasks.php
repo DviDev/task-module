b812f0937058e24e8f53b5951249961b051c213d
Davi Menezes <davimenezes.dev@gmail.com>
2024-11-20 16:16:04 -0300

    thread, files, message refactorings

--- Database/Migrations/2022_02_27_025209_create_tasks.php
+++ Database/Migrations/2022_02_27_025209_create_tasks.php
@@ -19,7 +19,7 @@ return new class extends Migration
 
             $p = TaskEntityModel::props(null, true);
 
-            $table->foreignId($p->record_id)->nullable()->references('id')->on('app_records')
+            $table->foreignId($p->record_id)->nullable()->references('id')->on('base_records')
                 ->cascadeOnUpdate()->restrictOnDelete();
 
             $table->foreignId($p->owner_id)->references('id')->on('users')
